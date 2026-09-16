be3a629e773d68b44b8ff4544486912a77341c1f
Praveen7986 <praveengandham151@gmail.com>
2026-09-16 21:16:16 +0530

    Static import in api/index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,18 +1,7 @@
-export default async function handler(req: any, res: any) {
-  try {
-    const { createJarvisApp } = await import("../server/createApp.js");
-    const app = createJarvisApp();
-    return app(req, res);
-  } catch (err: any) {
-    try {
-      const { createJarvisApp } = await import("../server/createApp");
-      const app = createJarvisApp();
-      return app(req, res);
-    } catch (innerErr: any) {
-      return res.status(200).json({
-        diagnosticError: innerErr?.message || String(innerErr),
-        stack: innerErr?.stack,
-      });
-    }
-  }
+import { createJarvisApp } from "../server/createApp";
+
+const app = createJarvisApp();
+
+export default function handler(req: any, res: any) {
+  return app(req, res);
 }
